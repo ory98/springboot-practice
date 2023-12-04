8afba50dae71c1820a9e41f33ce60d88d5da7932
ory98 <subin8360@naver.com>
2023-12-04 23:32:54 +0900

    첫번 째 커밋

---
+++ src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorMultiplyRequest.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.dto.calculator.request;
+
+public class CalculatorMultiplyRequest {
+    private int number1;
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    private int number2;
+}
